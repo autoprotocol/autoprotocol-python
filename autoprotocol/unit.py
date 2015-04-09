3abe9ea7bcb50515bf68d79b327f0df297e3177b
wolfram74 <phaugen@gmail.com>
2015-04-09 12:20:46 -0500

    used _check_type in __floordiv__

--- autoprotocol/unit.py
+++ autoprotocol/unit.py
@@ -94,12 +94,8 @@ class Unit(object):
         return Unit(self.value / other, self.unit)
 
     def __floordiv__(self, other):
-        if not isinstance(other, Unit):
-            raise ValueError("Both operands must be of type Unit")
-        elif self.unit != other.unit:
-            raise ValueError("unit %s is not %s" % (self.unit, other.unit))
-        else:
-            return Unit(self.value // other.value, self.unit)
+        self._check_type(other)
+        return Unit(self.value // other.value, self.unit)
 
     def __iadd__(self,other):
         if not isinstance(other, Unit):

1388ff3544fa9720ed96039777ab4758a6b6a286
wolfram74 <phaugen@gmail.com>
2015-04-09 12:08:31 -0500

    relocated _check_type to before arithmetic operations

--- autoprotocol/unit.py
+++ autoprotocol/unit.py
@@ -51,6 +51,12 @@ class Unit(object):
     def __str__(self):
         return ":".join([str(self.value), self.unit])
 
+    def _check_type(self, other):
+        if not isinstance(other, Unit):
+            raise ValueError("Both operands must be of type Unit")
+        elif self.unit != other.unit:
+            raise ValueError("unit %s is not %s" % (self.unit, other.unit))
+
     def __add__(self, other):
         if not isinstance(other, Unit):
             raise ValueError("Both operands must be of type Unit")
@@ -65,12 +71,6 @@ class Unit(object):
             raise ValueError("unit %s is not %s" % (self.unit, other.unit))
         else:
             return Unit(self.value - other.value, self.unit)
-
-    def _check_type(self, other):
-        if not isinstance(other, Unit):
-            raise ValueError("Both operands must be of type Unit")
-        elif self.unit != other.unit:
-            raise ValueError("unit %s is not %s" % (self.unit, other.unit))
 
     def __lt__(self, other):
         self._check_type(other)

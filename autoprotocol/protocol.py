c0685721ce7c98a89055c6a38fdfe4950e540ae3
Tali Herzka <tali@herzka.com>
2015-04-16 12:40:02 -0700

    add various pipette params and helper methods

--- autoprotocol/protocol.py
+++ autoprotocol/protocol.py
@@ -340,7 +340,10 @@ class Protocol(object):
 
     def distribute(self, source, dest, volume, allow_carryover=False,
                    mix_before=False, mix_vol=None, repetitions=10,
-                   flowrate="100:microliter/second"):
+                   flowrate="100:microliter/second", aspirate_speed=None,
+                   aspirate_source=None, distribute_target=None, pre_buffer=None,
+                   disposal_vol=None, transit_vol=None, blowout_buffer=None,
+                   tip_type=None):
         """
         Distribute liquid from source well(s) to destination wells(s)
 
@@ -453,6 +456,31 @@ class Protocol(object):
             liquid in a well before liquid is distributed.
         flowrate : str, Unit, optional
             Speed at which to mix liquid in well before liquid is distributed
+        aspirate speed : str, Unit, optional
+            Speed at which to aspirate liquid from source well.  May not be
+            specified if aspirate_source is also specified. By default this is the
+            maximum aspiration speed, with the start speed being half of the speed
+            specified.
+        aspirate_source : fn, optional
+            Can't be specified if aspirate_speed is also specified.
+        distribute_target : fn, optional
+            A function that contains additional parameters for distributing to
+            target wells including depth, dispense_speed, and calibrated volume.
+            If this parameter is specified, the same parameters will be applied to
+            every destination well.
+            Can't be specified if dispense_speed is also specified.
+        pre_buffer : str, Unit, optional
+            Volume of air aspirated before aspirating liquid
+        disposal_vol : str, Unit, optional
+            Volume of extra liquid to aspirate that will be dispensed into trash
+            afterwards
+        transit_vol : str, Unit, optional
+            Volume of air aspirated after aspirating liquid to reduce presence of
+            bubbles at pipette tip
+        blowout_buffer : bool, optional
+            If true the operation will dispense the pre_buffer along with the
+            dispense volume.
+            Cannot be true if disposal_vol is specified
 
         Raises
         ------
@@ -481,15 +509,206 @@ class Protocol(object):
                 opts["allow_carryover"] = allow_carryover
             opts["from"] = d["from"]
             opts["to"] = d["to"]
-            groups.append(
-                {"distribute": opts}
-            )
+            _assign(opts, "aspirate_speed", aspirate_speed)
+            _assign(opts, "allow_carryover", allow_carryover)
+            _assign(opts, "x_aspirate_source", aspirate_source)
+            _assign(opts, "x_pre_buffer", pre_buffer)
+            _assign(opts, "x_disposal_vol", disposal_vol)
+            _assign(opts, "x_transit_vol", transit_vol)
+            _assign(opts, "x_blowout_buffer", blowout_buffer)
+            _assign(opts, "x_tip_type", tip_type)
+
+            groups.append({"distribute": opts})
 
         self._pipette(groups)
 
+    def aspirate_source(depth=None, aspirate_speed=None, cal_volume=None,
+                        primer_vol=None):
+        '''
+        Set parameters for aspirating from source well before a transfer or
+        distribute.
+
+        Parameters
+        ----------
+        depth : fn, optional
+            Depth at which to aspirate liquid from a well.
+        aspirate_speed : dict
+            Dictionary specifying the "start" and "max" speed of aspiration
+        cal_volume : str, Unit, optional
+            Calibrated volume to aspirate from a well.
+        primer_vol : str, Unit
+            An amount of liquid that is aspirated in addition to the nominal volume
+            specified and then re-dispensed into the well the tip is aspirating from
+            (source well)
+        '''
+        source = {}
+        _assign(source, "depth", depth)
+        _assign(source, "aspirate_speed", aspirate_speed)
+        _assign(source, "volume", cal_volume)
+        _assign(source, "primer_vol", primer_vol)
+        return source
+
+    def dispense_target(depth=None, dispense_speed=None, cal_volume=None):
+        '''
+        Set parameters for dispensing to a target well during a transfer or
+        distribute.
+
+        Parameters
+        ----------
+        depth : fn, optional
+            Depth at which to dispense liquid into target well
+        dispense_speed : dict
+            Dictionary specifying the "start" and "max" dispense speed.
+            .. code-block:: json
+
+              {
+                "start": "50:microliver/second",
+                "max": "150:microliter/second"
+              }
+
+        cal_volume : str, Unit, optional
+            Calibrated volume to be dispensed to target well
+        '''
+        target = {}
+        _assign(target, "depth", depth)
+        _assign(target, "dispense_speed", dispense_speed)
+        _assign(target, "volume", cal_volume)
+        return target
+
+    def distribute_target(dst_loc, volume, dispense_speed=None, dispense_target=None):
+        '''
+        Set parameters target wells of a distrbute instruction.
+
+        Example usage:
+
+        .. code-block:: python
+
+            from autoprotocol.protocol import Protocol
+            from internal.xpipette import *
+            import json
+
+            p = Protocol()
+            sample_plate = p.ref("sample", None, "96-pcr", discard=True)
+
+            distribute_targets = []
+
+            distribute_targets.append(
+                distribute_target(sample_plate.well(1), "20:microliter",
+                                  dispense_speed="120:microliter/second",
+                                  dispense_target= dispense_target(depth=depth("ll_surface")))
+                )
+
+            distribute_targets.append(
+                distribute_target(sample_plate.well(2), "50:microliter",
+                                  dispense_speed="50:microliter/second")
+                )
+
+            p.append(Pipette([
+                            {"distribute": {
+                                "from": sample_plate.well(0),
+                                "to": distribute_targets
+                                }
+                            }]))
+
+        outputs:
+
+        .. code-block:: json
+
+            {
+              "refs": {
+                "sample": {
+                  "new": "96-pcr",
+                  "discard": true
+                }
+              },
+              "instructions": [
+                {
+                  "groups": [
+                    {
+                      "distribute": {
+                        "to": [
+                          {
+                            "volume": "20:microliter",
+                            "dispense_speed": "120:microliter/second",
+                            "well": "sample/1",
+                            "x_dispense_target": {
+                              "depth": {
+                                "method": "ll_surface"
+                              }
+                            }
+                          },
+                          {
+                            "volume": "50:microliter",
+                            "dispense_speed": "50:microliter/second",
+                            "well": "sample/2"
+                          }
+                        ],
+                        "from": "sample/0"
+                      }
+                    }
+                  ],
+                  "op": "pipette"
+                }
+              ]
+            }
+
+
+        Parameters
+        ----------
+        dst_loc : Well, str
+            Well (target) to distribute liquid to.
+        volume : str, unit
+            Nominal volume of liquid to dispense to the target well.
+        dispense_speed : dict
+            Dictionary specifying the "start" and "max" dispense speed
+            .. code-block:: json
+
+              {
+                "start": "50:microliver/second",
+                "max": "150:microliter/second"
+              }
+
+        dispense_target : fn, optional
+            May not be specified if dispense_speed is specified.  Allows further
+            configuration of dispense parameters such as depth.
+
+        '''
+        distribute = {
+            "well": dst_loc,
+            "volume": volume
+        }
+        _assign(distribute, "dispense_speed", dispense_speed)
+        _assign(distribute, "x_dispense_target", dispense_target)
+        return distribute
+
+    def depth(relation, lld = None, distance = None):
+        """
+        Return a stanza specifying pipette tip depth for aspirating or dispensing.
+
+        Parameters
+        ----------
+        relation : str
+          Relative position from which to measure distance of the pipette tip
+        lld : str, optional
+          Method of liquid level detection
+        distance : str, unit
+          Distance compared to position set by relation parameter, measured in millimeters
+
+        """
+        valid_depths = set(["ll_surface", "ll_following", "ll_top", "ll_bottom"])
+        if relation not in valid_depths:
+            raise RuntimeError("Invalid depth relation")
+        depth = {"method":relation}
+        _assign(depth,"lld",lld)
+        _assign(depth,"distance",distance)
+        return depth
+
     def transfer(self, source, dest, volume, one_source=False, one_tip=False,
                  mix_after=False, mix_before=False, mix_vol=None,
-                 repetitions=10, flowrate="100:microliter/second"):
+                 repetitions=10, flowrate="100:microliter/second",
+                 aspirate_speed=None, dispense_speed=None, aspirate_source=None,
+                 dispense_target=None, pre_buffer=None, disposal_vol=None,
+                 transit_vol=None, blowout_buffer=None, tip_type=None):
         """
         Transfer liquid from one specific well to another.  A new pipette tip
         is used between each transfer step unless the "one_tip" parameter
@@ -584,6 +803,29 @@ class Protocol(object):
         flowrate : str, Unit, optional
             Speed at which to mix liquid in well before and/or after each
             transfer step
+        aspirate speed : str, Unit, optional
+            Speed at which to aspirate liquid from source well.  May not be
+            specified if aspirate_source is also specified. By default this is the
+            maximum aspiration speed, with the start speed being half of the speed
+            specified.
+        dispense_speed : str, Unit, optional
+            Speed at which to dispense liquid into the destination well.  May not be
+            specified if dispense_target is also specified.
+        aspirate_source : fn, optional
+            Can't be specified if aspirate_speed is also specified.
+        dispense_target : fn, optional
+            Same but opposite of  aspirate_source
+        pre_buffer : str, Unit, optional
+            Volume of air aspirated before aspirating liquid
+        disposal_vol : str, Unit, optional
+            Volume of extra liquid to aspirate that will be dispensed into trash
+            afterwards
+        transit_vol : str, Unit, optional
+            Volume of air aspirated after aspirating liquid to reduce presence of
+            bubbles at pipette tip
+        blowout_buffer : bool, optional
+            If true the operation will dispense the pre_buffer along with the
+            dispense volume cannot be true if disposal_vol is specified
 
         Raises
         ------
@@ -649,6 +891,15 @@ class Protocol(object):
                     "repetitions": repetitions,
                     "speed": flowrate
                 }
+            _assign(xfer, "aspirate_speed", aspirate_speed)
+            _assign(xfer, "dispense_speed", dispense_speed)
+            _assign(xfer, "x_aspirate_source", aspirate_source)
+            _assign(xfer, "x_dispense_target", dispense_target)
+            _assign(xfer, "x_pre_buffer", pre_buffer)
+            _assign(xfer, "x_disposal_vol", disposal_vol)
+            _assign(xfer, "x_transit_vol", transit_vol)
+            _assign(xfer, "x_blowout_buffer", blowout_buffer)
+
             opts.append(xfer)
             if d.volume:
                 d.volume += v
@@ -656,11 +907,18 @@ class Protocol(object):
                 d.volume = v
             if s.volume:
                 s.volume -= v
+        trans = {}
+        assign(trans, "x_tip_type", tip_type)
         if one_tip:
-          self.append(Pipette([{"transfer": opts}]))
+            trans["transfer"] = opts
+            self.append(Pipette([trans]))
         else:
             for x in opts:
-                self._pipette([{"transfer": [x]}])
+                trans = {}
+                assign(trans, "x_tip_type", tip_type)
+                trans["transfer"] = [x]
+                self._pipette([trans])
+
 
     def stamp(self, source, dest, volume, quad=None, mix_before=False,
               mix_after=False, mix_vol=None, repetitions=10,
@@ -2294,7 +2552,6 @@ class Protocol(object):
             "weight": false           //default: false
           }, ... ]
 
-
       pos_controls : list of dicts, optional
           Optional list of positive control wells in the form of:
 
@@ -2476,6 +2733,10 @@ class Protocol(object):
             return str(op_data)
         else:
             return op_data
+
+    def _assign(obj, key, var):
+        if var is not None:
+            obj[key] = var
 
     def _ref_containers_and_wells(self, params):
         """

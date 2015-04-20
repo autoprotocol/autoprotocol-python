7903d33f8f5cb696ba5acc949de4538ce5a59ad2
Vanessa Biggers <vanessabiggers@gmail.com>
2015-04-20 12:08:38 -0700

    remove unsupported reagent references

--- autoprotocol/instruction.py
+++ autoprotocol/instruction.py
@@ -70,7 +70,7 @@ class Dispense(Instruction):
     ----------
     ref : Ref, str
         Container for reagent to be dispensed to.
-    reagent : {"water", "LB", "LB-amp", "LB-kan", "SOC", "PBS"}
+    reagent : str
         Reagent to be dispensed to columns in container.
     columns : list
         Columns to be dispensed to, in the form of a list of dicts specifying

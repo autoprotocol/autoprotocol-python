da95625e7f266609001f3779cb17920d4a0eca94
Tali Herzka <tali@herzka.com>
2015-08-28 13:12:49 -0700

    made **mix_kwargs encompass all possible mix_before and/or mix_after arguments

--- autoprotocol/protocol.py
+++ autoprotocol/protocol.py
@@ -601,12 +601,10 @@ class Protocol(object):
             self._pipette(groups)
 
     def transfer(self, source, dest, volume, one_source=False, one_tip=False,
-                 mix_after=False, mix_before=False, mix_vol=None,
-                 repetitions=10, flowrate="100:microliter/second",
                  aspirate_speed=None, dispense_speed=None,
                  aspirate_source=None, dispense_target=None, pre_buffer=None,
                  disposal_vol=None, transit_vol=None, blowout_buffer=None,
-                 tip_type=None, new_group=False):
+                 tip_type=None, new_group=False, **mix_kwargs):
         """
         Transfer liquid from one specific well to another.  A new pipette tip
         is used between each transfer step unless the "one_tip" parameter
@@ -839,10 +837,6 @@ class Protocol(object):
                                   new_group)
                 continue
 
-
-            if mix_after and not mix_vol:
-                mix_vol = v
-
             # Organize transfer options into dictionary (for json parsing)
             xfer = {
                 "from": s,
@@ -856,17 +850,17 @@ class Protocol(object):
                 d.volume = v
             if s.volume:
                 s.volume -= v
-            if mix_before:
+            if "mix_before" in mix_kwargs:
                 xfer["mix_before"] = {
-                    "volume": mix_vol,
-                    "repetitions": repetitions,
-                    "speed": flowrate
+                    "volume": mix_kwargs.get("mix_vol_b") or mix_kwargs.get("mix_vol") or v/2,
+                    "repetitions": mix_kwargs.get("repetitions_b") or mix_kwargs.get("repetitions") or 10,
+                    "speed":  mix_kwargs.get("flowrate_b") or  mix_kwargs.get("flowrate") or "100:microliter/second"
                 }
-            if mix_after:
+            if "mix_after" in mix_kwargs:
                 xfer["mix_after"] = {
-                    "volume": mix_vol,
-                    "repetitions": repetitions,
-                    "speed": flowrate
+                    "volume":  mix_kwargs.get("mix_vol_a") or mix_kwargs.get("mix_vol") or v/2,
+                    "repetitions": mix_kwargs.get("repetitions_a") or mix_kwargs.get("repetitions") or 10,
+                    "speed": mix_kwargs.get("flowrate_a") or  mix_kwargs.get("flowrate") or "100:microliter/second"
                 }
             # Append transfer options
             opt_list = ["aspirate_speed", "dispense_speed"]

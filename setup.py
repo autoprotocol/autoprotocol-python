4c905734d8990e701396bbefec37ef605b42671f
Tali Herzka <tali@herzka.com>
2015-10-12 16:59:28 -0700

    update version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Tali Herzka',
     description='Python library for generating Autoprotocol',
     author_email="tali@transcriptic.com",
-    version='2.4.1',
+    version='2.5.0',
     test_suite='test',
     packages=['autoprotocol']
 )

446f2b23023aa6cf86e2c16da54b75121671c89c
polarpine <vanessabiggers@gmail.com>
2016-06-17 14:54:14 -0700

    increment for release
    Summary: increment for 3.7.1 release
    
    Test Plan: tox
    
    Reviewers: cojofra
    
    Reviewed By: cojofra
    
    Differential Revision: https://work.r23s.net/D5082

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Vanessa Biggers',
     description='Python library for generating Autoprotocol',
     author_email="vanessa@transcriptic.com",
-    version='3.7.0',
+    version='3.7.1',
     test_suite='test',
     install_requires=[
         'Pint>=0.7.2'

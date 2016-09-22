a3f28a97a2f90db9d8b8da0022307a5c0d93d8f7
Jim Culver <jim@transcriptic.com>
2016-09-21 17:36:57 -0700

    Enable dyetests in autoprotocol.harness
    Summary: Run a dye test using the manifest preview when --dyetest is passed as an optional argument to harness.run
    
    Test Plan: -
    
    Differential Revision: https://work.r23s.net/D5990

--- autoprotocol/harness.py
+++ autoprotocol/harness.py
@@ -458,7 +458,7 @@ def _convert_provision_instructions(protocol, first_index, last_index, rs=_DYE_T
     if last_index < first_index:
         raise ValueError("last_index must be greater than or equal to first_index")
 
-    for instruction in protocol.instructions[first_instruction_index:last_instruction_index+1]:
+    for instruction in protocol.instructions[first_index:last_index+1]:
         if instruction.data["op"] == "provision":
             instruction.data["resource_id"] = rs
 

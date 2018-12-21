f82e6b6da9af97d6a6cef92306e822eea2a368fe
Rhys Ormond <rhysormond@gmail.com>
2018-12-21 14:01:36 -0800

    Wellgroup equality (#182)
    Implement equality for WellGroups

--- autoprotocol/container.py
+++ autoprotocol/container.py
@@ -227,6 +227,12 @@ class WellGroup(object):
         self.wells = wells
         self.name = None
 
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
     def set_properties(self, properties):
         """
         Set the same properties for each Well in a WellGroup.

db7b1988661ed5f34f1e28c5aeaf46388576f8ea
sreetej reddy <sreetej.nagarimadugu@zemosolabs.com>
2021-01-29 00:15:24 +0530

    feat: SEA-710 nit

--- autoprotocol/compound.py
+++ autoprotocol/compound.py
@@ -15,7 +15,7 @@ class Compound:
         if self.is_valid(InChI):
             self.InChI = InChI
         else:
-            raise CompoundError(f" {InChI} is not a valid InChI key")
+            raise CompoundError(f"{InChI} is not a valid InChI key")
 
     @staticmethod
     def is_valid(InChI):

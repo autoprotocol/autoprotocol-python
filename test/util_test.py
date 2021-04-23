2796abbc74ff89b27b14a265b2d69e9fb444efef
aaronstrateos <aaron.karlsberg@strateos.com>
2021-04-23 15:16:11 -0700

    define reservoir from attributes

--- test/util_test.py
+++ test/util_test.py
@@ -1,7 +1,8 @@
 import pytest
 
 from autoprotocol.unit import Unit
-from autoprotocol.util import parse_unit
+from autoprotocol.util import parse_unit, _is_reservoir, _check_container_type_with_shape
+from autoprotocol.container_type import _CONTAINER_TYPES
 
 
 class TestParseUnit(object):
@@ -25,3 +26,26 @@ class TestParseUnit(object):
 
         with pytest.raises(TypeError):
             parse_unit("1:ul", ["second", "kg"])
+
+
+    def test_reservoir(self):
+        container_type = _CONTAINER_TYPES["res-sw384-lp"]
+        shape_format = "SBS384"
+        assert _is_reservoir(container_type, shape_format) == True
+
+# For this to work I need to generate a legit shape, not sure how to do that.
+""" 
+from autoprotocol.builders import InstructionBuilders
+    # def test_check_container_type_with_shape(self):
+    #     container_type =  _CONTAINER_TYPES["res-sw384-lp"]
+    #     shape = InstructionBuilders.shape(self, rows=1, columns=1, format="SBS384")
+    #     _check_container_type_with_shape(container_type, shape)
+
+"""
+
+# a = TestParseUnit()
+
+# a.test_reservoir()
+
+# a.test_check_container_type_with_shape()
+

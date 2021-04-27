c0f4f348fbe83969b9e008e390c21eb7111e223d
aaronstrateos <aaron.karlsberg@strateos.com>
2021-04-27 14:43:28 -0700

    added test for container capabilities feature used in utils

--- test/util_test.py
+++ test/util_test.py
@@ -3,6 +3,9 @@ import pytest
 from autoprotocol.unit import Unit
 from autoprotocol.util import parse_unit
 
+from autoprotocol.container_type import _CONTAINER_TYPES
+from autoprotocol.instruction import LiquidHandle
+from autoprotocol.util import _check_container_type_with_shape
 
 class TestParseUnit(object):
     def test_casting_to_unit(self):
@@ -25,3 +28,10 @@ class TestParseUnit(object):
 
         with pytest.raises(TypeError):
             parse_unit("1:ul", ["second", "kg"])
+
+class TestUtil(object):
+    def test_compatible_reservoir_container(self):
+        container_type = _CONTAINER_TYPES["res-sw384-lp"]
+        container_type.capabilities.append("sbs384_compatible")
+        shape = LiquidHandle.builders.shape(1, 1, "SBS384")
+        _check_container_type_with_shape(container_type, shape)
